945d4008e46de2c14474c44dd0e29f27a0ff1e24
Magnus Henkel <magnus.henkel@zoho.com>
2014-10-30 19:36:22 -0600

    Algorimos de procesos terminado!!!!

--- alg_process_management/alg_process_management.py
+++ alg_process_management/alg_process_management.py
@@ -7,94 +7,52 @@ Created on Oct 20, 2014
 
 @summary: 
     Programa que crea una serie de proceso con un determinado instante de inicio y una duración
-    (ambos aleatorios) y que evalua los parámetros de 'T', 'E' y 'P'. Se define:
-    T = tiempo que toma el trabajo con tiempo de espera incluido
-    E = tiempo en espera
-    P = Fracción de tiempo de respuesta durante P estuvo en espera.
+    (ambos aleatorios) y que evalua los parámetros de 'Total', 'Idle' y 'Penalización'. Se define:
+    Total = tiempo que toma el trabajo con tiempo de espera incluido
+    Idle = tiempo en espera
+    Penalización = Fracción de tiempo de respuesta durante P estuvo en espera.
+    
+    El programa arroja 3 estadísticas para los 3 algoritmos y calcula los promedios para el total, 
+    el tiempo idle y la penalización. 
+    
+    La creación de los procesos es aleatoria, es decir que los tiempos de llegada y ejecución se 
+    determinan aleatoriamente. Sin embargo, los procesos vienen en orden, es decir que el proceso 
+    A llega antes que B, B antes que C, etc.
 '''
 
 from random import randrange
-from time import sleep
 
-'''
-def print_table(process_list):
-    for p in process_list:
-        print(p.name, '\t\t  ', p.time_of_arrival, '\t\t     ', p.execution_time)
-    print()
-'''
+def testdummy(inp_list):
+    '''
+    La clase testdummy(list) sirve para testing.
+    Recibe una lista con valres de los que se conozca el resultado y con esto se compruebe el correcto
+    funcionamiento del programa.
+    '''
+    # comantar esta lista para pasar una lista propia
+    inp_list = [(0, 6), (1, 10), (4, 4), (9, 9), (12, 8)]
 
-def testdummy():
     p_list = []
-    A = VirtualProcess(0, 6)
+    A = VirtualProcess(*inp_list[0])
     A.name = 'A'
     p_list.append(A)
 
-    B = VirtualProcess(1, 10)
+    B = VirtualProcess(*inp_list[1])
     B.name = 'B'
     p_list.append(B)
 
-    C = VirtualProcess(4, 4)
+    C = VirtualProcess(*inp_list[2])
     C.name = 'C'
     p_list.append(C)
 
-    D = VirtualProcess(9, 9)
+    D = VirtualProcess(*inp_list[3])
     D.name = 'D'
     p_list.append(D)
 
-    E = VirtualProcess(12, 8)
+    E = VirtualProcess(*inp_list[4])
     E.name = 'E'
     p_list.append(E)
     
     return p_list
-
-def testdummy1():
-    p_list = []
-    A = VirtualProcess(0, 3)
-    A.name = 'A'
-    p_list.append(A)
-
-    B = VirtualProcess(6, 15)
-    B.name = 'B'
-    p_list.append(B)
-
-    C = VirtualProcess(11, 14)
-    C.name = 'C'
-    p_list.append(C)
-
-    D = VirtualProcess(13, 3)
-    D.name = 'D'
-    p_list.append(D)
-
-    E = VirtualProcess(15, 14)
-    E.name = 'E'
-    p_list.append(E)
-    
-    return p_list
-
-def testdummy2():
-    p_list = []
-    A = VirtualProcess(0, 11)
-    A.name = 'A'
-    p_list.append(A)
-
-    B = VirtualProcess(7, 11)
-    B.name = 'B'
-    p_list.append(B)
-
-    C = VirtualProcess(10, 3)
-    C.name = 'C'
-    p_list.append(C)
-
-    D = VirtualProcess(17, 13)
-    D.name = 'D'
-    p_list.append(D)
-
-    E = VirtualProcess(21, 6)
-    E.name = 'E'
-    p_list.append(E)
-    
-    return p_list
-
 
 class VirtualProcess():
     '''
@@ -120,7 +78,10 @@ def create_virtual_processes():
     procesos posteriores. Esto asegura que proceso B no llegue antes que proceso A, etc
     '''
     global wait_time_all
-    #global hook_in 
+    # la variable hook_in (enganche) guarda el límite máximo del rango de valores del proceso anterior
+    # y lo pone como límite mínimo para el tiempo de llegada para siguiente proceso. Así se 
+    # mantiene el orden de llegada. Los nombres de los procesos se asignan dinámicamente a 
+    # través del código ASCII
     hook_in = 0
     process_list = []
 
@@ -158,13 +119,13 @@ def caller():
         process_list = create_virtual_processes()
 
         first_comes_first_served(process_list)
-        statistics(process_list, "\nFirst Comes First Served")
+        statistics(process_list, "\n++++++ First Comes First Served ++++++")
 
         round_robin(process_list)
-        statistics(process_list, "\nRound Robin")
+        statistics(process_list, "\n++++++ Round Robin +++++++")
         
         shortest_next(process_list)
-        statistics(process_list, "\nShortest Process Next")
+        statistics(process_list, "\n++++++ Shortest Process Next ++++++")
         
         print()
         print('=' * 65)
@@ -174,8 +135,12 @@ def first_comes_first_served(process_list):
     '''
     Algoritmo First-Comes-First-Served
     '''
+    # variable que guarda el tiempo de ejecución y idle time
+    # de todos los procesos anteriores, es efectivamente el 
+    # tiempo de espera para el último proceso 
     wait_time_all = 0
 
+    # se copia la lista para no tocar el original
     process_list_copy = process_list[:]
 
     while process_list_copy:
@@ -184,10 +149,12 @@ def first_comes_first_served(process_list):
         # calcula el tiempo que haya tenido que esperar un proceso hasta que sea atendido
         current_process.wait_time = wait_time_all - current_process.time_of_arrival
 
+        # si los tiempos de espera son negativos quiere decir que el proceso no tuvo que esperar
         if current_process.wait_time < 0:
             current_process.wait_time = 0
         wait_time_all = wait_time_all + current_process.execution_time
 
+        # se actualizan las estadísticas
         current_process.tot_time = current_process.wait_time + current_process.execution_time 
         current_process.t_resp_wait = (current_process.tot_time / current_process.execution_time) 
 
@@ -196,10 +163,6 @@ def round_robin(process_list):
     '''
     Algortimo Round Robin
     '''
-
-    # variable que guarda el tiempo de ejecución y idle time
-    # de todos los procesos anteriores, es efectivamente el 
-    # tiempo de espera para el último proceso 
     wait_time_all = 0
 
     process_list_copy = process_list[:]
@@ -207,21 +170,30 @@ def round_robin(process_list):
     while process_list_copy:
         current_process = process_list_copy.pop(0)
         
+        # se comprueba si un proceso tuvo que esperar o no.
+        # si hubo espera entonces se calcula el hueco (idle time) y se  adelanta el tiempo de
+        # llegada del proceso
         if wait_time_all < current_process.time_of_arrival:
             current_process.time_of_arrival = current_process.time_of_arrival - \
                 (current_process.time_of_arrival - wait_time_all)
          
+        # si el valor de la operacón anterior es positivo quiere decir que va a ejecutar
+        # los 4 quantums completos
         if current_process.execution_countdown > 4:
             current_process.execution_countdown = current_process.execution_countdown - 4
             process_list_copy.append(current_process)
             wait_time_all = wait_time_all + 4
             
 
+        # si el valor de la operación anterior es negativo se calcula el tiempo que le queda 
+        # al proceso y solamente estos quantums se suma al tiempo total de ejecución
         elif current_process.execution_countdown < 4:
             current_process.execution_countdown = current_process.execution_countdown - 4
             wait_time_all = wait_time_all + (4 - abs(current_process.execution_countdown))
             current_process.execution_countdown = 0
 
+        # si el valor de la operación anterior es  igual a 4 (el número de quantums por 
+        # tiempo de ejecución) entonces ya terminó este proceso
         elif current_process.execution_countdown == 4:
             wait_time_all = wait_time_all + 4
             current_process.execution_countdown = 0
@@ -230,58 +202,49 @@ def round_robin(process_list):
         current_process.wait_time = current_process.tot_time - current_process.execution_time
         current_process.t_resp_wait = current_process.tot_time / current_process.execution_time
 
-def print_plist(plist, message): 
-    print(message)
-    for item in plist:
-        print("Name: ", item.name, "Arr.Time", item.time_of_arrival, "Exec.Time", item.execution_time)
-
 def shortest_next(process_list):
     '''
-    TODO: Algoritmo Shortest Process Next
+    Algoritmo Shortest Process Next
     '''
-    
     wait_time_all = 0
     process_list_copy = process_list[:]
-    
-    #print_plist(process_list_copy, "\nAfter processs_list_copy assignment")
 
     while process_list_copy:
         concurrent_processes = []
+        # se ejecuta el primer proceso de la lista
         current_process = process_list_copy.pop(0)
-        #print("-------")
-        #print("\nCurrent before check: ", current_process.name)
-        #print("-------")
+        # los demás procesos se agregan a una lista nueva
         other_processes = process_list_copy[:]
-        #print_plist(other_processes, "Others unsorted: ")
 
+        # se calcula el tiempo de espera para el proceso en ejecución
         current_process.wait_time = wait_time_all - current_process.time_of_arrival
         
+        # si los tiempos de espera son negativos quiere decir que el proceso no tuvo que esperar
         if current_process.wait_time < 0:
             current_process.wait_time = 0
             
+        # se actualizan las estadísticas para el proceso en ejecution, después éste desaparece para siempre
         current_process.tot_time = current_process.execution_time + current_process.wait_time
         current_process.t_resp_wait = current_process.tot_time / current_process.execution_time
         
         wait_time_all = wait_time_all + current_process.execution_time
-        #print('\nInside while after wait_time_all set: ', wait_time_all)
 
+        # para el proceso en ejecución se checa los procesos que son concurrentes.
+        # y se agregan a una lista
         for item in other_processes: 
-            #print("Inside for, Item name: ", item.name)
-            #if item.time_of_arrival < wait_time_all and item.time_of_arrival > wait_time_all_before_current:
             if item.time_of_arrival < wait_time_all:
                 concurrent_processes.append(item)
-        #        print(item.name, " appended")
 
+        # se ordena la lista de procesos concurrentes del proceso en ejecución por el criterio del
+        # tiempo de ejecución. Ahora se sabe que el primero de la lista es el siguiente que se 
+        # va a ejecutar. Este proceso se inserta nuevamente a la lista de procesos en primer lugar. 
+        # Por eso sabemos que va a ser elegido como siguiente.
         concurrent_processes.sort(key = lambda x: x.execution_time)
-        #print("\nconcurrent processes of process: ", current_process.name)
         
-        #print_plist(concurrent_processes, "Concurrent processes: ")
-
         if concurrent_processes:
             dispatched_process = concurrent_processes.pop(0)
             process_list_copy.remove(dispatched_process)
             process_list_copy.insert(0, dispatched_process) 
-        #print_plist(process_list_copy, "New process_list_copy")
 
 def statistics(process_list, message):
     '''
@@ -290,36 +253,23 @@ def statistics(process_list, message):
     que se ejecutó justo antes.
      '''
     t_average = 0
-    e_average = 0
+    i_average = 0
     p_average = 0
 
     print(message,'\n')
-    print('Virtual    Time of    Execution    T         E            P\nProcess    Arrival    Time')
-    print("_" * 65)
+    print('Virtual    Time of    Execution   Total     Idle    Penalty\nProcess    Arrival    Time')
+    print("_" * 60)
     for p in process_list:
-        print(p.name, '\t  ', p.time_of_arrival, '\t     ', p.execution_time, '\t ', p.tot_time, '\t    ', p.wait_time, ' \t', '%.4f'%(p.t_resp_wait))
+        print(p.name, '\t  ', p.time_of_arrival, '\t     ', p.execution_time, '     \t ', p.tot_time, '\t   ', p.wait_time, '\t   ', '%.4f'%(p.t_resp_wait))
         t_average = t_average + float(p.tot_time)
-        e_average = e_average + float(p.wait_time)
+        i_average = i_average + float(p.wait_time)
         p_average = p_average + float(p.t_resp_wait)
 
-    print('_' * 65)
-    print('Promedio: \t\t\t ',  '%.2f'%(t_average / 5), '    ', '%.2f'%(e_average / 5), '\t', '%.2f'%(p_average / 5))
-
+    print('_' * 60)
+    print('Promedio:              ', '\t',  '%.2f'%(t_average / 5), '\t  ', '%.2f'%(i_average / 5), '  ', '%.2f'%(p_average / 5))
 
 '''
 Main
 '''
-
 process_list = create_virtual_processes()
 caller()
-
-#process_listB = testdummy1()
-#first_comes_first_served(process_listB)
-#statistics(process_listB, 'tst')
-
-#process_listA = testdummy2()
-#round_robin(process_listA)
-#shortest_next(process_listA)
-#statistics(process_listA, 'test')
-
-#statistics(process_listA, 'test')
