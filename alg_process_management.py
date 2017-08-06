1d7d365312b34e1791711ab9cb1765fdff76d992
Magnus Henkel <magnus.henkel@zoho.com>
2017-08-06 16:06:48 -0500

    Comments

--- alg_process_management.py
+++ alg_process_management.py
@@ -1,33 +1,28 @@
-'''
-Created on Oct 20, 2014
+"""
 
-@authors: 
-    Garrido Alan
-    Henkel Magnus
+Programa que crea una serie de proceso con un determinado instante de inicio y una duración
+(ambos aleatorios) y que evalua los parámetros de 'Total', 'Idle' y 'Penalización'. Se define:
+Total = tiempo que toma el trabajo con tiempo de espera incluido
+Idle = tiempo en espera
+Penalización = Fracción de tiempo de respuesta durante P estuvo en espera.
 
-@summary: 
-    Programa que crea una serie de proceso con un determinado instante de inicio y una duración
-    (ambos aleatorios) y que evalua los parámetros de 'Total', 'Idle' y 'Penalización'. Se define:
-    Total = tiempo que toma el trabajo con tiempo de espera incluido
-    Idle = tiempo en espera
-    Penalización = Fracción de tiempo de respuesta durante P estuvo en espera.
-    
-    El programa arroja 3 estadísticas para los 3 algoritmos y calcula los promedios para el total, 
-    el tiempo idle y la penalización. 
-    
-    La creación de los procesos es aleatoria, es decir que los tiempos de llegada y ejecución se 
-    determinan aleatoriamente. Sin embargo, los procesos vienen en orden, es decir que el proceso 
-    A llega antes que B, B antes que C, etc.
-'''
+El programa arroja 3 estadísticas para los 3 algoritmos y calcula los promedios para el total, 
+el tiempo idle y la penalización. 
+
+La creación de los procesos es aleatoria, es decir que los tiempos de llegada y ejecución se 
+determinan aleatoriamente. Sin embargo, los procesos vienen en orden, es decir que el proceso 
+A llega antes que B, B antes que C, etc.
+
+"""
 
 from random import randrange
 
 def testdummy(inp_list):
-    '''
+    """
     La clase testdummy(list) sirve para testing.
     Recibe una lista con valres de los que se conozca el resultado y con esto se compruebe el correcto
     funcionamiento del programa.
-    '''
+    """
     # comentar esta lista para pasar una lista propia
     inp_list = [(0, 6), (1, 10), (4, 4), (9, 9), (12, 8)]
 
@@ -55,11 +50,11 @@ def testdummy(inp_list):
     return p_list
 
 class VirtualProcess():
-    '''
+    """
     La clase VirtualProcess sirve solamente para guardar los valores de cada proceso virtual. Se
     inicializa cada objeto de un proceso virtual con dos parámetros: el tiempo de llegada y el
     tiempo que se va a ejecutar. El nombre se le da posteriormente para la impresión de las estadísticas.
-    '''
+    """
     def __init__(self, time_of_arrival, execution_time):
         self.name = '' 
         self.time_of_arrival = time_of_arrival
@@ -70,13 +65,13 @@ class VirtualProcess():
         self.t_resp_wait = 0
 
 def create_virtual_processes():
-    '''
+    """
     Esta función crea los procesos virtuales con valores de tiempo de llegada y de tiempo de
     ejecución aleatorios. Se crean 5 procesos y se les da nombres desde A a E, A llega primero,
     después B, C, D y al final E.
     La variable hook_in determina el valor a partir del cual se generan tiempos de llegada para
     procesos posteriores. Esto asegura que proceso B no llegue antes que proceso A, etc
-    '''
+    """
     global wait_time_all
     # la variable hook_in (enganche) guarda el límite máximo del rango de valores del proceso anterior
     # y lo pone como límite mínimo para el tiempo de llegada para siguiente proceso. Así se 
@@ -94,23 +89,23 @@ def create_virtual_processes():
     return process_list
 
 def randomize_process_parameters(start_val, process_list):
-    '''
+    """
     Función auxiliar a la función create_virtual_processes().
     Aquí es donde ocurre la generación de los valores mimos. Si la lista de procesos está vacia
     quiere decir que llega el proceso A que no tiene que esperar ya que llega primero. Por lo tanto
     el tiempo de llega es '0'
-    '''
+    """
     if not process_list:
         return (0, randrange(15) + 3)
     else:
         return (randrange(start_val, start_val + 7), randrange(15) + 3)
 
 def caller():
-    '''
+    """
     Función que llama a los diferentes algoritmos con la misma lista de procesos.
     Al final de cada llamada de un algoritmo llama a la función 'statistics' para
     imprimir los resultados y cálculos.
-    '''
+    """
     global process_list
     
     print('=' * 65)
@@ -132,9 +127,9 @@ def caller():
         
 
 def first_comes_first_served(process_list):
-    '''
+    """
     Algoritmo First-Comes-First-Served
-    '''
+    """
     # variable que guarda el tiempo de ejecución y idle time
     # de todos los procesos anteriores, es efectivamente el 
     # tiempo de espera para el último proceso 
@@ -160,9 +155,9 @@ def first_comes_first_served(process_list):
 
 
 def round_robin(process_list):
-    '''
+    """
     Algortimo Round Robin
-    '''
+    """
     wait_time_all = 0
 
     process_list_copy = process_list[:]
@@ -203,9 +198,9 @@ def round_robin(process_list):
         current_process.t_resp_wait = current_process.tot_time / current_process.execution_time
 
 def shortest_next(process_list):
-    '''
+    """
     Algoritmo Shortest Process Next
-    '''
+    """
     wait_time_all = 0
     process_list_copy = process_list[:]
 
@@ -247,11 +242,11 @@ def shortest_next(process_list):
             process_list_copy.insert(0, dispatched_process) 
 
 def statistics(process_list, message):
-    '''
+    """
     La función 'statistics' imprime un mensaje de que algoritmo se trata y formatea la lista de
     procesos. Un proceso en este momento de la impresión tiene valores actualizados de un algoritmo
     que se ejecutó justo antes.
-     '''
+     """
     t_average = 0
     i_average = 0
     p_average = 0
@@ -268,8 +263,8 @@ def statistics(process_list, message):
     print('_' * 60)
     print('Promedio:              ', '\t',  '%.2f'%(t_average / 5), '\t  ', '%.2f'%(i_average / 5), '  ', '%.2f'%(p_average / 5))
 
-'''
+"""
 Main
-'''
+"""
 process_list = create_virtual_processes()
 caller()
